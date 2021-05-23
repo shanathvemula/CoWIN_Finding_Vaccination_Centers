b99362586e4c1e9355d1e91343eadb1f13974670
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2021-05-23 10:25:31 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import json
 import requests
 
 
@@ -20,7 +19,7 @@ def index(request):
         URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, date1)
         header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
         result = requests.get(URL,headers=header)
-        rjson = json.loads(result.text)
+        rjson = eval(result.text)
         print(type(rjson))
         if result.ok:
             return render(request,'index.html',{'rjson':rjson})
